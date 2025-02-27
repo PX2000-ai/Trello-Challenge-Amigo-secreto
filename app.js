24220146f5260f8a949d94f4a0c17455bfe68b67
PX2000-ai <pedrosampaio@id.uff.br>
2025-02-27 12:24:46 -0300

    Nova versão

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ let listaDeNomes = [];
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
     if (nome == "") {
-    alert('Por favor ensira um nome válido a lista');
-     } else {
-    listaDeNomes.push(nome);
-    document.querySelector('input').value = '';
-    listaAmigos.innerHTML = listaDeNomes;};
+      alert('Por favor ensira um nome válido a lista');
+      return;
+     } 
+    if (/^\d+$/.test(nome)) {
+      alert("Por favor, digite um nome válido (não pode conter apenas números).");
+      return;
+     }    
+    if (listaDeNomes.includes(nome)) {
+      alert("Este nome já foi adicionado à lista de amigos.");
+    } else {
+        listaDeNomes.push(nome);
+        document.querySelector('input').value = '';
+        listaAmigos.innerHTML = listaDeNomes;
+        button.innerText = 'novo';};
 };
 
 function sortearAmigo(){
-    let quantidadeDeElementosNaLista = listaDeNomes.length;
-    let escolhido = (parseInt(Math.random()*quantidadeDeElementosNaLista));
+    let escolhido = (parseInt(Math.random()*listaDeNomes.length));
     if (listaDeNomes != '') {
-    resultado.innerHTML = (listaDeNomes[escolhido]);
-    listaDeNomes.splice(escolhido,1);
-    listaAmigos.innerHTML = listaDeNomes;
+       resultado.innerHTML = (listaDeNomes[escolhido]);
+       listaDeNomes.splice(escolhido,1);
+       listaAmigos.innerHTML = listaDeNomes;
     } else {
-    resultado.innerHTML = '';
-    return adicionarAmigo();
+       resultado.innerHTML = '';
+       return adicionarAmigo();
     };
 };
+
+document.querySelector("button-add").innerHTML = 'novo';
